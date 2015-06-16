a15cede0583923bfebf9238c929381211c311b15
Oguz Huner <oguzcanhuner@gmail.com>
2015-06-16 16:39:40 +0100

    add known_fields for campaign

--- lib/eucalyptus/campaign.rb
+++ lib/eucalyptus/campaign.rb
@@ -3,6 +3,12 @@ require_relative 'resource'
 module Eucalyptus
   class Campaign < Resource
     def self.known_fields
+      [
+        :name,
+        :status,
+        :users,
+        :accounts
+      ]
     end
 
     def self.api_path
