d468c7f6c3c1b340d0a786a5e57bbe7d8589a23a
Oguz Huner <oguzcanhuner@gmail.com>
2015-06-17 09:48:42 +0100

    add known_fields to ad_set

--- lib/eucalyptus/ad_set.rb
+++ lib/eucalyptus/ad_set.rb
@@ -3,6 +3,13 @@ require_relative 'resource'
 module Eucalyptus
   class AdSet < Resource
     def self.known_fields
+      [
+        :buying_type,
+        :campaign_group_status,
+        :name,
+        :objective,
+        :spend_cap
+      ]
     end
 
     def self.api_path
