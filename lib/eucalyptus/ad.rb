f781780ddcdf53240bd376309c24125be13ac7a7
Oguz Huner <oguzcanhuner@gmail.com>
2015-06-17 09:53:08 +0100

    add known_fields to ad

--- lib/eucalyptus/ad.rb
+++ lib/eucalyptus/ad.rb
@@ -3,6 +3,26 @@ require_relative 'resource'
 module Eucalyptus
   class Ad < Resource
     def self.known_fields
+      [
+        :account_id,
+        :bid_info,
+        :bid_type,
+        :budget_remaining,
+        :campaign_group_id,
+        :campaign_status,
+        :created_time,
+        :creative_sequence,
+        :daily_budget,
+        :end_time,
+        :is_autobid,
+        :lifetime_budget,
+        :name,
+        :promoted_object,
+        :rf_prediction_id,
+        :start_time,
+        :targeting,
+        :updated_time
+      ]
     end
 
     def self.api_path
