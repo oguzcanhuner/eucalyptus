cfcbcab11354e9f5001db2df8937d8d28c495e7e
Oguz Huner <oguzcanhuner@gmail.com>
2015-06-18 11:12:08 +0100

    remove simplecov

--- Gemfile
+++ Gemfile
@@ -11,7 +11,6 @@ group :development do
   gem "rdoc", "~> 3.12"
   gem "bundler", "~> 1.0"
   gem "jeweler", "~> 2.0.1"
-  gem "simplecov", ">= 0"
   gem "pry"
   gem 'dotenv'
   gem 'webmock'
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,22 +1,6 @@
 require 'dotenv'
 Dotenv.load
 
-require 'simplecov'
-
-module SimpleCov::Configuration
-  def clean_filters
-    @filters = []
-  end
-end
-
-SimpleCov.configure do
-  clean_filters
-  load_adapter 'test_frameworks'
-end
-
-ENV["COVERAGE"] && SimpleCov.start do
-  add_filter "/.rvm/"
-end
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 
