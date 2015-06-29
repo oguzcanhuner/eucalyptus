023b73d4c5fbff0180c05176d6c352508460f448
Oguz Huner <oguzcanhuner@gmail.com>
2015-06-29 10:42:37 +0100

    add ad_sets endpoint to custom audiences

--- lib/eucalyptus/custom_audience.rb
+++ lib/eucalyptus/custom_audience.rb
@@ -6,6 +6,14 @@ module Eucalyptus
       "customaudiences"
     end
 
+    def ad_sets
+      AdSet.all.collect do |ad_set|
+        if ad_set.targeting.custom_audiences
+          ad_set if ad_set.targeting.custom_audiences.any?{|a| a["id"] == self.id }
+        end
+      end.compact
+    end
+
     def self.known_fields
       [:name, :description]
     end
--- spec/eucalyptus/custom_audience_spec.rb
+++ spec/eucalyptus/custom_audience_spec.rb
@@ -1,4 +1,19 @@
 require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
 describe Eucalyptus::CustomAudience do
+  before do
+    Eucalyptus.configure do |config|
+      config.access_token = ENV["ACCESS_TOKEN"]
+    end
+  end
+
+  describe '#ad_sets' do
+    it 'returns ad_sets which belong to the audience' do
+      VCR.use_cassette('audience_ad_sets') do
+        audience = Eucalyptus::CustomAudience.all.first
+        ad_sets = audience.ad_sets
+        expect(ad_sets.first.targeting.custom_audiences.first['id']).to eql audience.id
+      end
+    end
+  end
 end
