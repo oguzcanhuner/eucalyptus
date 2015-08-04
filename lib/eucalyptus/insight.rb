4908bb5dee4ebf9d1d3d2b8329b03f35133125b7
Oguz Huner <oguzcanhuner@gmail.com>
2015-08-04 11:59:06 +0100

    there seems to be a limit on number of fields you can pass

--- lib/eucalyptus/insight.rb
+++ lib/eucalyptus/insight.rb
@@ -3,7 +3,33 @@ require_relative 'resource'
 module Eucalyptus
   class Insight < Resource
     def self.known_fields
-      nil
+     [
+        :call_to_action_clicks,
+        :clicks,
+        :cost_per_total_action,
+        :cpc,
+        :cost_per_unique_click,
+        :cpm,
+        :cpp,
+        :ctr,
+        :website_ctr,
+        :unique_ctr,
+        :frequency,
+        :impressions,
+        :reach,
+        :relevance_score,
+        :social_clicks,
+        :unique_social_clicks,
+        :social_impressions,
+        :social_reach,
+        :spend,
+        :total_action_value,
+        :total_actions,
+        :total_unique_actions,
+        :cost_per_action_type,
+        :action_values,
+        :actions
+      ]
     end
 
     def self.find
